d740950cd33dbbc26a2f589953ae34be5f780873
jaidev7823 <jaidev7823@gmail.com>
2025-09-05 13:24:13 +0530

    feat: added location from react dom so when user change from today to calender heading change to date to month

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Header from '../components/Header/Header';
 
 export default function MainLayout() {
+  const location = useLocation();
+  const currentPage = location.pathname.slice(1) || 'today';
+
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-        <Header />
-        <main className="flex-1 overflow-y-auto bg-slate-900">
+        <Header currentPage={currentPage} />
+        <main className="flex-1 overflow-y-auto p-8 bg-slate-900">
           <Outlet />
         </main>
       </div>
